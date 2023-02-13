0ad0db4a0322bbd89c7dadad328d22d6b216ac69
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-02-13 09:54:38 -0600

    Submited solution

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,10 +1,17 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    try:
+        in_num  = int(input("Enter a number: "))
+        absolute = in_num - 21
+        absolute = abs(absolute)
+        if in_num > 21:
+            absolute *= 2
+        print('Result: ' + str(absolute))
+    except:
+        print('Please enter a number')
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
-
 if __name__ == "__main__":
     calculateAbsolute()
